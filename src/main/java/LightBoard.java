2afc31316f79442d3dfc905d69d451635e31d35c
astro-phel <katgranads@gmail.com>
2025-06-03 08:56:35 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -13,7 +13,7 @@ public class LightBoard
     /* to be implemented in part (a) */
     lights = new boolean[numRows][numCols];
     for(int r = 0; r < numRows; r++)
-      for(int c = 0; c < numRows; c++)
+      for(int c = 0; c < numCols; c++)
         double rnd = Math.random();
     lights[r][c] = rnd < 0.4;
   }
