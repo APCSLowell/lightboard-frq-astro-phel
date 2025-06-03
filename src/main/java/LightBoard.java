0dfb9b423b986f3615c1e4a46976b2bec546d73a
astro-phel <katgranads@gmail.com>
2025-06-03 10:09:02 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -8,33 +8,54 @@ public class LightBoard
    * Precondition: numRows > 0, numCols > 0
    * Postcondition: each light has a 40% probability of being set to on.
    */
-  public LightBoard(int numRows, int numCols){
-    lights = new boolean[numRows][numCols];
-    for (int r = 0; r < numRows; r++){
-      for (int c = 0; c < numCols; c++){
-        double rnd = Math.random();
-        lights[r][c] = rnd < 0.4;
+  public LightBoard(int numRows, int numCols)
+  {
+    /* to be implemented in part (a) */
+  lights = new boolean[numRows][numCols];
+    for(int r = 0; r < numRows; r++){
+      for(int c = 0; c < numCols; c++){
+        double chance = Math.random();
+        if(chance < 0.4){
+          lights[r][c] = true;
+        }
       }
     }
   }
+
   /** Evaluates a light in row index row and column index col and returns a status
    *  as described in part (b).
    *  Precondition: row and col are valid indexes in lights.
    */
   public boolean evaluateLight(int row, int col){
-    int numOn = 0;
-    for (int r = 0; r < lights.length; r++){
-      if (lights[r][col]){
-        numOn++;
-      }
-    }
-    if (lights[row][col] && numOn % 2 == 0){
-      return false;
-    }
-    if (!lights[row][col] && numOn % 3 == 0){
-      return true;
-    }
-    return lights[row][col];
+    /* to be implemented in part (b) */
+   if(lights[row][col] == true){
+     int onCount = 0;
+     for(int r = 0; r < lights.length; r++){
+       if(lights[r][col] == true){
+         onCount++;
+       }
+       if(onCount % 2 == 0){
+         return false;
+       }
+       if(lights[row][col] == false){
+         int numOn = 0;
+       }
+       for(int rowOn = 0; rowOn < lights.length; rowOn++){
+         if(lights[rowOn][col] == true){
+           numOn++;
+         }
+         if(numOn % 3 == 0){
+           return true;
+         }
+         boolean toReturn = lights[row][col];
+         return toReturn;
+       }
+     }
+   }
+  }
+  public boolean[][] getLights()
+  {
+    return lights;
   }
   //used for testing
   public String toString()
